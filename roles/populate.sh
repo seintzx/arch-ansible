c300d31955dd7c84efeae3e21319a680e064c99a
seintzx <seintzx@gmail.com>
2020-01-30 17:31:14 +0100

    remove populate script

--- roles/populate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-MACRO=(android archive awesome base bluetooth browser chat clipboard cron ctf dotfiles filesystem fonts lockscreen multimedia network office python shell sound theme torrent utils virtual xorg)
-
-for DIRE in ${MACRO[@]}
-do
-    mkdir -p ${DIRE}/files ${DIRE}/tasks ${DIRE}/templates ${DIRE}/handlers ${DIRE}/meta
-    touch ${DIRE}/tasks/main.yml
-done
